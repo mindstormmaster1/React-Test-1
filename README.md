2b2fb8269304279a26979c00bbc991f4d5af694b
mindstormmaster1 <mindstormmaster1@gmail.com>
2022-02-22 15:43:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-nu9cgv
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-nu9cgv)
---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+import Button from '@mui/material/Button';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <row>
+      <Button variant="contained">Contained</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="contained">Contained</Button>
+      </row>
+      <Button variant="contained">Contained</Button>
+    </div>
+  );
+}
